0cba11e5b6759216562d6ed3e85a3f23c4b07213
Bruce Sheplan <bruce@sheplan.com>
2024-08-14 19:40:07 -0500

    Fixed error on timestamp measurement and fixed linting errors.

--- custom_components/mysutro/sensor.py
+++ custom_components/mysutro/sensor.py
@@ -1,12 +1,13 @@
+""" Represents a sensor entity for the Sutro device """
+
+import datetime
 import logging
-from xmlrpc.client import Boolean
 
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers import entity_platform, config_validation as cv
+from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 
+import dateutil.parser
 from . import mySutroEntity
 from .const import DOMAIN, PROP_MAP
-import dateutil.parser
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,6 +28,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
 
 class mySutroSensor(mySutroEntity, SensorEntity):
+    """ Represents a sutro sensor """
     def __init__(self, coordinator, data_key):
         super().__init__(coordinator, data_key)
         self.native_min_value = PROP_MAP[data_key]['min']
@@ -44,6 +46,7 @@ class mySutroSensor(mySutroEntity, SensorEntity):
 
     @property
     def value(self) -> float:
+        """ Returns the value of the sensor """
         # return self.gateway.data.me.pool.latestReading
         return self.gateway.data[self._data_key]
 
@@ -54,14 +57,16 @@ class mySutroSensor(mySutroEntity, SensorEntity):
 
     @property
     def data_valid(self):
+        """ Returns true is data is valid, always true """
         return True
 
 
 class mySutroTimeStamp(mySutroEntity, SensorEntity):
+    """ Represents a timestamp """
     def __init__(self, coordinator, data_key):
         super().__init__(coordinator, data_key)
-        self._attr_state_class = 'measurement'
-        self._attr_device_class = 'timestamp'
+        # self._attr_state_class = 'measurement'
+        self._attr_device_class = SensorDeviceClass.TIMESTAMP
 
     @property
     def name(self) -> str:
@@ -72,16 +77,18 @@ class mySutroTimeStamp(mySutroEntity, SensorEntity):
         return f"{super().unique_id}_{self._data_key}"
 
     @property
-    def value(self) -> float:
+    def value(self) -> datetime.datetime:
+        """ Returns value as timestamp """
         # return self.gateway.data.me.pool.latestReading
         # lastReadTime = dateutil.parser.parse(self.gateway.data[self._data_key])
         return dateutil.parser.parse(self.gateway.data[self._data_key])
 
     @property
-    def native_value(self) -> float:
-        # return self.gateway.data.me.pool.latestReading
-        return dateutil.parser.parse(self.gateway.data[self._data_key])
+    def native_value(self) -> datetime.datetime:
+        """ Returns value as timestamp """
+        return self.value
 
     @property
     def data_valid(self):
+        """ Returns true is data is valid, always true """
         return True
